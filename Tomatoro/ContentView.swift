1d854df18db900d17d1b05c00ab8dc1d0a750d60
VerticalHeretic <ls.warss98@gmail.com>
2023-03-02 15:45:31 +0100

    feat: Initial commit, with initial tomato

--- Tomatoro/ContentView.swift
+++ Tomatoro/ContentView.swift
@@ -6,16 +6,98 @@
 //
 
 import SwiftUI
+import Combine
+
+struct Constants {
+    static let timerLength = 1 * 60
+}
 
 struct ContentView: View {
+
+    @StateObject var viewModel = ContentViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("🍅")
+                .font(.largeTitle)
+
+            switch viewModel.status {
+            case .notStarted:
+                Button {
+                    viewModel.start()
+                } label: {
+                    Text("Start")
+                }
+            case .inProgress:
+                HStack {
+                    Text("\(viewModel.remainingSeconds / 60):\(String(format: "%02d", viewModel.remainingSeconds % 60))")
+                        .font(.largeTitle)
+                }
+            case .complete:
+                Button {
+                    viewModel.restart()
+                } label: {
+                    Text("Restart")
+                }
+            }
         }
         .padding()
+    }
+}
+
+final class ContentViewModel: ObservableObject {
+
+    enum Status {
+        case notStarted
+        case inProgress
+        case complete
+    }
+
+    var timerCancellable: AnyCancellable?
+    @Published var status: Status = .notStarted
+    @Published var startDate: Date?
+    @Published var remainingSeconds = Constants.timerLength
+
+    func start() {
+        status = .inProgress
+        startDate = Date()
+
+        startTimer()
+    }
+
+    func complete() {
+        status = .complete
+    }
+
+    func restart() {
+        status = .inProgress
+        startDate = Date()
+        startTimer()
+    }
+
+    private func startTimer() {
+        timerCancellable = Timer
+            .publish(every: 1,
+                     on: .current,
+                     in: .common)
+            .autoconnect()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] _ in
+                guard let self = self else { return }
+
+                if self.status == .inProgress {
+                    if let startDate = self.startDate {
+                        let elapsedTime = Date().timeIntervalSince(startDate)
+                        let remainingTime = max(0, Constants.timerLength - Int(elapsedTime))
+                        self.remainingSeconds = remainingTime
+                        if remainingTime == 0 {
+                            self.status = .complete
+                        }
+                    }
+                } else if self.status == .notStarted {
+                    self.start()
+                }
+            })
     }
 }
 
