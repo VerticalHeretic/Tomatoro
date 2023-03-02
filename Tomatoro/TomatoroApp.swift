ab4d5d199dd6aee5edc60c8af8af829edbb2c9d2
VerticalHeretic <ls.warss98@gmail.com>
2023-03-02 14:52:03 +0100

    Initial Commit

---
+++ Tomatoro/TomatoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoroApp.swift
+//  Tomatoro
+//
+//  Created by Łukasz Stachnik on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TomatoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
